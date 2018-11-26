e94204740094d97ef37c5e307822f06ff289f2e1
rafiadji <rafiadji13@gmail.com>
2018-11-26 08:49:14 +0700

    edit spider

--- tarantula.py
+++ tarantula.py
@@ -1,12 +1,12 @@
 import scrapy
 
 class BlogSpider(scrapy.Spider):
-    name = 'blogspider'
-    start_urls = ['https://blog.scrapinghub.com']
+    name = 'Tribunnews'
+    start_urls = ['http://www.tribunnews.com/tag/malang']
 
     def parse(self, response):
-        for title in response.css('.post-header>h2'):
-            yield {'title': title.css('a ::text').extract_first()}
+        for title in response.css('div.mr140>h3'):
+            yield {'title': title.css('a::text').extract_first()}
 
-        for next_page in response.css('div.prev-post > a'):
-            yield response.follow(next_page, self.parse)
+        # for next_page in response.css('div.prev-post > a'):
+        #     yield response.follow(next_page, self.parse)
