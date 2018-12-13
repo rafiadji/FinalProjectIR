af31f6552250a36a1c53b701c75c55091f4b5c08
Bima Indra <bimaindra444@gmail.com>
2018-12-13 21:17:12 +0700

    menambahkan countword, stopword dan perhitungan TF

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+	$d = [
+		"Roses are red violet is blue your face is red, my face is blue",
+		"Roses are red violet is blue so that my love is only for you"
+	];
+
+	echo "TF(red, D1) : ".tf("red",$d[0])."<br>";
+	echo "TF(blue, D2) : ".tf("blue",$d[1])."<br>";
+
+	function countword($d) {
+		return str_word_count($d);
+	}
+
+	function stopword($word) {
+		$symbol = [".",",","?","(",")"];
+
+		// proses menghilangkan simbol
+		for($i=0; $i<count($symbol); $i++) {
+			$word = str_replace($symbol[$i], "", $word);
+		}
+
+		return $word;
+	}
+
+	function tf($word,$d) {
+		$count = 0;
+		$res_stw = stopword($d);
+		$exp = explode(" ",strtolower($res_stw));
+		for($i=0; $i<countword($d); $i++) {
+			if($exp[$i] == $word) {
+				$count++;
+			}
+		}
+
+		return $count / countword($d);
+	}
+?>
