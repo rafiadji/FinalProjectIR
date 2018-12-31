2bb81fb0dd293a1e68839d3f96eef177cb3d82ca
rafiadji <rafiadji13@gmail.com>
2018-12-31 11:09:18 +0700

    tambah database

--- sylvia.py
+++ sylvia.py
@@ -1,12 +1,21 @@
+import pymysql
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
+# Begin Database
+host = "localhost"
+user = "root"
+password = ""
+db = "db_clickbait"
+
+conn = pymysql.connect(host, user, password, db)
+cursor = conn.cursor()
+# End Database
+
+# Begin Selenium
 browser = webdriver.Chrome()
-
-# tribunnews
-
 browser.get('http://www.tribunnews.com/tag/malang')
 
 def getData():
@@ -16,7 +25,6 @@ def getData():
         print(judul)
 
 pages_remaining = True
-
 getData()
 while pages_remaining:
     try:
@@ -26,27 +34,6 @@ while pages_remaining:
     except NoSuchElementException:
         pages_remaining = False
         browser.quit()
-# detik.com
-
-# browser.get('https://news.detik.com/')
-
-# judulLinks = browser.find_elements_by_css_selector('.desc_nhl>a>h2')
-# for judulLink in judulLinks :
-#     judul = judulLink.text
-#     print(judul)
-
-# Reference
-
-# browser.get('http://stiki.ac.id/')
-
-# searchLink = browser.find_element_by_class_name('fusion-main-menu-icon')
-# searchLink.click()
-
-# searchInput = browser.find_element_by_class_name('s')
-# searchInput.send_keys('kemahasiswaan' + Keys.RETURN)
-
-# judulLinks = browser.find_elements_by_css_selector('h2.entry-title>a')
-# for judulLink in judulLinks :
-#     judul = judulLink.text
-#     print(judul)
-# browser.quit()
+# End Selenium
+conn.commit()
+conn.close()
