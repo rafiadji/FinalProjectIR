9bf9f03eb8215ba373ebc64b836a8a7e2fd0ca02
Bima Indra <bimaindra444@gmail.com>
2019-01-18 06:40:05 +0700

    read_doc, insert unique word, dan s training data

--- index.php
+++ index.php
@@ -11,6 +11,34 @@
 	echo "TF.IDF(love, D) : ".tf_idf("face",$d[0],$d)."<br>";
 	echo "TF.IDF(love, D) : ".tf_idf("face",$d[1],$d)."<br>";
 	echo tf_idf($d);
+	
+	// Baca tabel dokumen
+	function readDokumen() {
+		include("koneksi.php");
+		$resDoc = [];
+
+		$sql = mysqli_query($con, "SELECT * FROM document");
+		while($res = mysqli_fetch_assoc($sql)) {
+			array_push($resDoc, strtolower($res['document']));
+		}
+
+		return $resDoc;
+	}
+
+	// Insert Unique Word
+	function insertUniqueWord($word) {
+		include("koneksi.php");
+		for($i=0; $i<count($word); $i++) {
+			$res_word = $word[$i]['word'];
+			if($res_word == "" || $res_word == " ") {} else {
+				$detAvWord = mysqli_query($con, "SELECT * FROM unique_word WHERE uword='".$res_word."'");
+				$detAvWord = mysqli_num_rows($detAvWord);
+				if($detAvWord == 0) {
+					mysqli_query($con, "INSERT INTO unique_word VALUES('','$res_word')");
+				}
+			}
+		}
+	}
 
 	function countword($d) {
 		return str_word_count($d);
@@ -72,4 +100,47 @@
 		$idf = idf($word,$D);
 		return $tf * $idf;
 	}
+
+	function countUniqueWords($wd) {
+		$word = "";
+		for($i=0; $i<count($wd); $i++) { 
+			$word .= strtolower($wd[$i])." ";
+		}
+
+		$word = rtrim($word," ");
+		$word = stopword($word);
+		$exp = explode(" ", $word);
+		$wordNew = [];
+		$wordfinal = [];
+		foreach ($exp as $row) {
+			array_push($wordNew, $row);
+		}
+
+		$unique = array_unique($wordNew);
+		foreach($unique as $count) {
+			$found = array_keys($wordNew, $count);
+			
+			array_push($wordfinal, array("word" => $count, "count" => count($found)));
+		}
+
+		return $wordfinal;
+	}
+	
+	// simple training data
+	include("koneksi.php");
+	mysqli_query($con, "TRUNCATE unique_word");
+
+	$d1 = "Heboh Pria di Jagakarsa Gantung Diri Sambil Live Facebook";
+	$d2 = "Jamaah Umroh asal Gresik Meninggal saat Tiba di Bandara Juanda";
+
+	$doc = readDokumen();
+	$test = [$d1,$d2,$doc[49]];
+	$all_doc = [];
+
+	for($i=0; $i<count($test); $i++) { 
+		array_push($all_doc, stopword($test[$i]));
+	}
+
+	$un = countUniqueWords($test);
+	insertUniqueWord($un);
 ?>
